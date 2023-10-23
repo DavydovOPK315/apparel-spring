e6b56c1502b6de27144da1f6f459c45b9b2abeb9
Denys Davydov <davydov654@gmail.com>
2023-10-23 19:35:43 +0300

    initial commit

---
+++ src/main/java/com/teamchallenge/ApparelSpringApplication.java
@@ -0,0 +1,13 @@
+package com.teamchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApparelSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApparelSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamchallenge/ApparelSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApparelSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
